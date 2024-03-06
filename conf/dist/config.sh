b7a779892a61b4a6c4f7b2e91d006057db684130
Andra <andra778@yahoo.com>
2024-03-06 16:56:04 +0700

    DB assembler

--- conf/dist/config.sh
+++ conf/dist/config.sh
@@ -3,11 +3,9 @@
 SRCPATH="$AC_PATH_ROOT"
 
 # absolute path where build files must be stored
-BUILDPATH=${BUILDPATH:-"$AC_PATH_VAR/build/obj"}
+BUILDPATH="$AC_PATH_ROOT/var/build/obj"
 
-# absolute path where azerothcore will be installed
-# NOTE: on linux the binaries are stored in a subfolder (/bin)
-# of the $BINPATH
+# absolute path where binary files must be stored
 BINPATH="$AC_PATH_ROOT/env/dist"
 
 # bash fills it by default with your os type. No need to change it.
@@ -17,33 +15,19 @@ BINPATH="$AC_PATH_ROOT/env/dist"
 # When using linux, our installer automatically get information about your distro
 # using lsb_release. If your distro is not supported but it's based on ubuntu or debian,
 # please change it to one of these values.
-# OSDISTRO="ubuntu"
+#OSDISTRO="ubuntu"
 
 # absolute path where config. files must be stored
 # default: the system will use binpath by default
 # CONFDIR="$AC_PATH_ROOT/env/dist/etc/"
-
-# absolute path where maps and client data will be downloaded
-# by the AC dashboard
-# default: the system will use binpath by default
-# DATAPATH="$BINPATH/bin"
-# DATAPATH_ZIP="$DATAPATH/data.zip"
-
-# azerothcore's official remote source address to pull from
-# by default git will fetch form the azrothcore remote
-# You can change it to "origin" if you want to fetch/pull from the set remote
-ORIGIN_REMOTE="https://github.com/azerothcore/azerothcore-wotlk.git"
-
-# Branch configuration for the installer to pull from.
-# By default git will select the current working branch
-# You can set it to "master" if you want the latest updates
-INSTALLER_PULL_FROM=
 
 ##############################################
 #
 #  COMPILER_CONFIGURATIONS
 #
 ##############################################
+
+
 # Set preferred compilers.
 # To use gcc (not suggested) instead of clang change in:
 #  CCOMPILERC="/usr/bin/gcc"
@@ -52,99 +36,148 @@ INSTALLER_PULL_FROM=
 CCOMPILERC="/usr/bin/clang"
 CCOMPILERCXX="/usr/bin/clang++"
 
+
 # how many thread must be used for compilation ( leave zero to use all available )
-MTHREADS=${MTHREADS:-0}
+MTHREADS=0
 # enable/disable warnings during compilation
 CWARNINGS=ON
-
 # enable/disable some debug informations ( it's not a debug compilation )
 CDEBUG=OFF
-
-# specify compilation type:
-# * Release: high optimization level, no debug info, code or asserts.
-# * Debug: No optimization, asserts enabled, [custom debug (output) code enabled],
-#    debug info included in executable (so you can step through the code with a
-#    debugger and have address to source-file:line-number translation).
-# * RelWithDebInfo: optimized, *with* debug info, but no debug (output) code or asserts.
-# * MinSizeRel: same as Release but optimizing for size rather than speed.
-CTYPE=${CTYPE:-Release}
-
+# specify compilation type
+CTYPE=Release
 # compile scripts
-CSCRIPTS=${CSCRIPTS:-static}
-
-# compile modules
-CMODULES=${CMODULES:-none}
-
+CSCRIPTS=ON
 # compile unit tests
-CBUILD_TESTING=OFF
-
+CUNIT_TESTS=OFF
+# compile server
+CSERVERS=ON
+# compile tools
+CTOOLS=OFF
 # use precompiled headers ( fatest compilation but not optimized if you change headers often )
-CSCRIPTPCH=${CSCRIPTPCH:-ON}
-CCOREPCH=${CCOREPCH:-ON}
+CSCRIPTPCH=ON
+CCOREPCH=ON
 
-# build apps list variable
-CAPPS_BUILD=${CAPPS_BUILD:-all}
-
-# build tools list variable
-CTOOLS_BUILD=${CTOOLS_BUILD:-none}
-
-# build apps list
-CBUILD_APPS_LIST=${CBUILD_APPS_LIST:-''}
-
-# build tools list
-CBUILD_TOOLS_LIST=${CBUILD_TOOLS_LIST:-''}
-
-# github ci define
-CCONTINUOUS_INTEGRATION=${CCONTINUOUS_INTEGRATION:-OFF}
+# Skip specific modules from compilation (cmake reconfigure needed)
+# use semicolon ; to separate modules
+CDISABLED_AC_MODULES=""
 
 # you can add your custom definitions here ( -D )
-# example:  CCUSTOMOPTIONS=" -DWITH_PERFTOOLS=ON
+# example:  CCUSTOMOPTIONS=" -DWITH_PERFTOOLS=ON -DENABLE_EXTRA_LOGS=ON"
 #
-CCUSTOMOPTIONS=${CCUSTOMOPTIONS:-''}
+CCUSTOMOPTIONS=""
 
-# Enable ccache to speedup
-# recompilations
-#
-AC_CCACHE=${AC_CCACHE:-false}
-export CCACHE_DIR=${CCACHE_DIR:-"$AC_PATH_VAR/ccache"}
 
 ##############################################
 #
-#  GOOGLE PERF TOOLS
-#
-#  Repository: https://github.com/gperftools/gperftools#readme
-#  Documentation: https://gperftools.github.io/gperftools/
-#
-# Install (Ubuntu):
-#  sudo apt-get install google-perftools libgoogle-perftools-dev
-# Note: dependencies above are already installed in docker
-#
-# Usage:
-#  1. To enable the gperftools you need to compile with the -DWITH_PERFTOOLS=ON compiler flag. You can use CCUSTOMOPTIONS above to set it for the dashboard compiler
-#  2. Configure the variable below accordingly to your needs
-#  3. run the worldserver with the "./acore.sh run-worldserver"
-#  4. run "killall -12 worldserver"  This command will start the monitoring process. Run "killall -12 worldserver" again to stop the process when you want
-#  5. At this time you will have the .prof file ready in the folder configured below.
-#  Run "google-pprof --callgrind <path/of/worldserver/bin> </path/of/prof/file>" This will generate a callgrind file that can be read with
-#  QCacheGrind, KCacheGrind and any other compatible tools
+#  DB ASSEMBLER / EXPORTER CONFIGURATIONS
 #
 ##############################################
 
-# files used by gperftools to store monitored information
-export CPUPROFILE=${CPUPROFILE:-"$BINPATH/logs/worldserver-cpu.prof"}
-# heap profile is disabled by default. Uncomment this line to enable it
-# export HEAPPROFILE=${HEAPPROFILE:-"$BINPATH/logs/worldserver-heap.prof"}
+#
+# Basically you don't have to edit it
+# but if you have another database you can add it here
+# and create relative confiugurations below
+#
+DATABASES=(
+	"AUTH"
+	"CHARACTERS"
+	"WORLD"
+)
 
-# signal to send to the kill command to start/stop the profiling process. kill -12
-export CPUPROFILESIGNAL=${CPUPROFILESIGNAL:-12}
+OUTPUT_FOLDER="$AC_PATH_ROOT/env/dist/sql/"
 
-# How many interrupts/second the cpu-profiler samples.
-#export CPUPROFILE_FREQUENCY=${CPUPROFILESIGNAL:-100}
+####### BACKUP
+# Set to true if you want to backup your azerothcore databases before importing the SQL with the db_assembler
+# Do not forget to stop your database software (mysql) before doing so
 
-# If set to any value (including 0 or the empty string), use ITIMER_REAL instead of ITIMER_PROF to gather profiles.
-# In general, ITIMER_REAL is not as accurate as ITIMER_PROF, and also interacts badly with use of alarm(),
-# so prefer ITIMER_PROF unless you have a reason prefer ITIMER_REAL.
-#export CPUPROFILE_REALTIME=${CPUPROFILE_REALTIME}
+BACKUP_ENABLE=false
 
-# Other values for HEAPCHECK: minimal, normal (equivalent to "1"), strict, draconian
-#export HEAPCHECK=${HEAPCHECK:-normal}
+BACKUP_FOLDER="$AC_PATH_ROOT/env/dist/sql/backup/"
+
+#######
+
+# FULL DB
+DB_AUTH_PATHS=(
+    $SRCPATH"/data/sql/base/db_auth/"
+)
+
+DB_CHARACTERS_PATHS=(
+    $SRCPATH"/data/sql/base/db_characters"
+)
+
+DB_WORLD_PATHS=(
+    $SRCPATH"/data/sql/base/db_world/"
+)
+
+# UPDATES
+DB_AUTH_UPDATES_PATHS=(
+    $SRCPATH"/data/sql/updates/db_auth/"
+    $SRCPATH"/data/sql/updates/pending_db_auth/"
+)
+
+DB_CHARACTERS_UPDATES_PATHS=(
+    $SRCPATH"/data/sql/updates/db_characters/"
+    $SRCPATH"/data/sql/updates/pending_db_characters/"
+)
+
+DB_WORLD_UPDATES_PATHS=(
+    $SRCPATH"/data/sql/updates/db_world/"
+    $SRCPATH"/data/sql/updates/pending_db_world/"
+)
+
+# CUSTOM
+DB_AUTH_CUSTOM_PATHS=(
+    $SRCPATH"/data/sql/custom/db_auth/"
+)
+
+DB_CHARACTERS_CUSTOM_PATHS=(
+    $SRCPATH"/data/sql/custom/db_characters/"
+)
+
+DB_WORLD_CUSTOM_PATHS=(
+    $SRCPATH"/data/sql/custom/db_world/"
+)
+
+##############################################
+#
+#  DB EXPORTER/IMPORTER CONFIGURATIONS
+#
+##############################################
+
+#
+# Skip import of base sql files to avoid
+# table dropping
+#
+DB_SKIP_BASE_IMPORT_IF_EXISTS=true
+
+#
+# Example:
+#        "C:/Program Files/MySQL/MySQL Server 5.6/bin/mysql.exe"
+#        "/usr/bin/mysql"
+#        "mysql"
+#
+
+DB_MYSQL_EXEC="mysql"
+DB_MYSQL_DUMP_EXEC="mysqldump"
+
+
+DB_AUTH_CONF="MYSQL_USER='acore'; \
+                    MYSQL_PASS='acore'; \
+                    MYSQL_HOST='localhost';\
+                    "
+
+DB_CHARACTERS_CONF="MYSQL_USER='acore'; \
+                    MYSQL_PASS='acore'; \
+                    MYSQL_HOST='localhost';\
+                    "
+
+DB_WORLD_CONF="MYSQL_USER='acore'; \
+                    MYSQL_PASS='acore'; \
+                    MYSQL_HOST='localhost';\
+                    "
+
+DB_AUTH_NAME="acore_auth"
+
+DB_CHARACTERS_NAME="acore_characters"
+
+DB_WORLD_NAME="acore_world"
